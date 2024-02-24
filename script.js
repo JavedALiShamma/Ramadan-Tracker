381f6153dccc025cecf5439131da5f45d2c6d5bb
Javed Ali <javedshamma@gmail.com>
2024-02-24 21:31:00 +0530

    New version

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ dragIndicator.addEventListener("click",function(){
  let daysCol=document.querySelector(".days-col");
  daysCol.classList.toggle("display-off");
 })
+// Here we need a API to calculate the date of the system
+// Step 1 we need the date
+const dateToday=new Date();
+// Now we have to check the following 
+if(dateToday.getDate()==24 && (dateToday.getMonth()+1)==2){
+    /// Here we need to add roza Day now 
+    let rozaNumber=document.getElementById("roza-number");
+    rozaNumber.innerHTML="2";
+}
+
